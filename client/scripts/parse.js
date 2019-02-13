87d459ad14d7070a49741676a8a2836d8565d808
Bruce Johnson <bruce@yogainternational.com>
2019-02-12 23:51:33 -0500

    adds strategy to implement create method

--- client/scripts/parse.js
+++ client/scripts/parse.js
@@ -4,6 +4,25 @@ var Parse = {
 
   create: function(message, successCB, errorCB = null) {
     // todo: save a message to the server
+
+    /*
+    ? The main goal is to built this object:
+    var message = {
+      username: 'shawndrost',
+      text: 'trololo',
+      roomname: '4chan'
+    };
+
+    -Check for room selected by .roomSelector
+      -If no room selected return popup telling the user to select a room
+    -Check for username in the URL
+    -Check for message in #message input
+
+    Fill in message obj with the data gathered above
+
+    Submit POST request through AJAX. Code found on Learn.
+
+    */
   },
 
   readAll: function(successCB, errorCB = null) {
