9673b471f4b3eb764267f319937f0f02c48288cd
Bruce Johnson <bruce@yogainternational.com>
2019-02-12 23:26:01 -0500

    Complete displaying messages from parse server

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,5 +1,4 @@
 var App = {
-
   $spinner: $('.spinner img'),
 
   username: 'anonymous',
@@ -14,14 +13,73 @@ var App = {
     // Fetch initial batch of messages
     App.startSpinner();
     App.fetch(App.stopSpinner);
-
   },
 
-  fetch: function(callback = ()=>{}) {
+  fetch: function(callback = () => {}) {
     Parse.readAll((data) => {
       // examine the response from the server request:
       console.log(data);
 
+      //iterate over data.results
+      // var count = 0;
+
+      var _escapeChars = function(string) {
+        var newString;
+        var stringArr = [];
+        // iterate over the characters item.text
+        for (var i = 0; i < string.length; i++) {
+          // string.charAt(i);
+          // if the current char is &
+          if (string.charAt(i) === '&') {
+            // replace with &amp;
+            stringArr.push('&amp;');
+          }
+          // if the current char is <
+          if (string.charAt(i) === '<') {
+            // replace with &lt;
+            stringArr.push('&lt;');
+          }
+          // if the current char is >
+          if (string.charAt(i) === '>') {
+            // replace with &gt;
+            stringArr.push('&gt;');
+          }
+          // if the current char is "
+          if (string.charAt(i) === '"') {
+            // replace with &quot;
+            stringArr.push('&quot;');
+          }
+          // if the current char is '
+          if (string.charAt(i) === `'`) {
+            // replace with &#x27;
+            stringArr.push('&#x27;');
+          }
+          // if the current char is /
+          if (string.charAt(i) === '/') {
+            // replace with &#x2F;
+            stringArr.push('&#x2F;');
+          } else {
+            stringArr.push(string.charAt(i));
+          }
+        }
+        newString = stringArr.join('').toString();
+        return newString;
+      };
+
+      data.results.forEach((item) => {
+        // pass each item into the messageView.render and append the result to #chats div
+        // count++;
+        // console.log(item, count);
+        if (!item.username) {
+          item.username = 'anonymous';
+        }
+        if (item.text) {
+          item.username = _escapeChars(item.username);
+          item.text = _escapeChars(item.text);
+          $('#chats').append(MessageView.render(item));
+        }
+      });
+      App.stopSpinner();
       callback();
     });
   },
@@ -34,5 +92,5 @@ var App = {
   stopSpinner: function() {
     App.$spinner.fadeOut('fast');
     FormView.setStatus(false);
-  }
+  },
 };
