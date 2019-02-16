f857e77360bb0e208d0182ab4838a256f5ffc12e
Bruce Johnson <bruce@yogainternational.com>
2019-02-16 15:57:28 -0500

    fixed character escape bug

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -14,7 +14,6 @@ var App = {
     // Fetch initial batch of messages
     App.startSpinner();
     App.fetch(App.stopSpinner);
-    // Friends.initialize();
   },
 
   fetch: function(callback = () => {}, selectedRoom = 'all') {
@@ -25,91 +24,38 @@ var App = {
       //iterate over data.results
       // var count = 0;
 
-      var _escapeChars = function(string) {
-        var newString;
-        var stringArr = [];
-        // iterate over the characters item.text
-        for (var i = 0; i < string.length; i++) {
-          // string.charAt(i);
-          // if the current char is &
-          if (string.charAt(i) === '&') {
-            // replace with &amp;
-            stringArr.push('&amp;');
-          }
-          // if the current char is <
-          if (string.charAt(i) === '<') {
-            // replace with &lt;
-            stringArr.push('&lt;');
-          }
-          // if the current char is >
-          if (string.charAt(i) === '>') {
-            // replace with &gt;
-            stringArr.push('&gt;');
-          }
-          // if the current char is "
-          if (string.charAt(i) === '"') {
-            // replace with &quot;
-            stringArr.push('&quot;');
-          }
-          // if the current char is '
-          if (string.charAt(i) === `'`) {
-            // replace with &#x27;
-            stringArr.push('&#x27;');
-          }
-          // if the current char is /
-          if (string.charAt(i) === '/') {
-            // replace with &#x2F;
-            stringArr.push('&#x2F;');
-          } else {
-            stringArr.push(string.charAt(i));
-          }
-        }
-        newString = stringArr.join('').toString();
-        return newString;
-      };
-
       data.results.forEach((item) => {
-        // pass each item into the messageView.render and append the result to #chats div
-        // count++;
-        // console.log(item, count);
-
         if (!item.username) {
           item.username = 'anonymous';
         }
         if (selectedRoom !== 'all') {
           if (item.text && item.roomname === selectedRoom) {
-            item.username = _escapeChars(item.username);
-            item.text = _escapeChars(item.text);
+            item.username = _.escape(item.username);
+            item.text = _.escape(item.text);
             $('#chats').append(MessageView.renderWithRoomName(item));
             // check to see if the username exists on the Friends obj
-            // console.log(Friends.friendsObj, Friends.friendsObj[`${item.username}`], item.username);
             if (Friends.friendsObj[`${item.username}`]) {
-              // console.log(item.username, ' is in friends list');
               // add highlight style to $('.username:contains(item.username)').css("color", "light-blue");
-              $(`.username:contains('${item.username}')`).css('color', 'red');
+              $(`.username:contains('${item.username}')`).css('color', 'dodgerBlue');
             }
           }
         } else {
+          item.username = _.escape(item.username);
+          item.text = _.escape(item.text);
           if (item.text && !item.roomname) {
-            item.username = _escapeChars(item.username);
-            item.text = _escapeChars(item.text);
             $('#chats').append(MessageView.render(item));
             // check to see if the username exists on the Friends obj
-            // console.log(Friends.friendsObj, Friends.friendsObj[`${item.username}`], item.username);
             if (Friends.friendsObj[`${item.username}`]) {
-              // console.log(item.username, ' is in friends list');
               // add highlight style to $('.username:contains(item.username)').css("color", "light-blue");
-              $(`.username:contains('${item.username}')`).css('color', 'red');
+              $(`.username:contains('${item.username}')`).css('color', 'dodgerBlue');
             }
           }
           if (item.roomname && item.text) {
             $('#chats').append(MessageView.renderWithRoomName(item));
             // check to see if the username exists on the Friends obj
-            // console.log(Friends.friendsObj, Friends.friendsObj[`${item.username}`], item.username);
             if (Friends.friendsObj[`${item.username}`]) {
-              // console.log(item.username, ' is in friends list');
               // add highlight style to $('.username:contains(item.username)').css("color", "light-blue");
-              $(`.username:contains('${item.username}')`).css('color', 'red');
+              $(`.username:contains('${item.username}')`).css('color', 'dodgerBlue');
             }
           }
         }
